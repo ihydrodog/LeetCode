075690a042fd8765581324a0aa9d33c1cb2b98e7
hydro <ihydrodog@gmail.com>
2019-01-04 22:02:06 +0900

    first try

--- Minimum Window Substring.py
+++ Minimum Window Substring.py
@@ -1,4 +1,8 @@
+from collections import Counter
+
+
 class Solution:
+
     def minWindow(self, s, t):
         """
         :type s: str
@@ -7,6 +11,7 @@ class Solution:
         """
 
         posMap = {}
+        freq = Counter(t)
 
         lenS = len(s)
         lenT = len(t)
@@ -17,25 +22,27 @@ class Solution:
         for index in range(lenS):
             c = s[index]
             if c in t:
-                prevIndex = posMap.get(c, None)
-                if prevIndex != None:
-                    indexQueue.remove(prevIndex)
+                indexes = posMap.get(c, None)
+                if indexes != None and len(indexes) == freq[c]:
+                    indexQueue.remove(indexes.pop(0))
 
                 indexQueue.append(index)
 
-                posMap[c] = index
+                posMap.setdefault( c, [] ).append( index )
 
                 minIndex = indexQueue[0]
 
-                if len(posMap) == lenT:
+                if len(indexQueue) == lenT:
                     if index - minIndex < minWindowWidth:
                         minWindowIndex = minIndex
                         minWindowWidth = index - minIndex
 
         return s[minWindowIndex:minWindowIndex + minWindowWidth + 1] if minWindowIndex != None else ''
 
+print( Solution().minWindow( "bbaa", "aba" ))
 
-
+print( Solution().minWindow( "BB", "BB" ))
 
 print( Solution().minWindow( "ADOBECODEBANC", "ABC" ))
+
 
