46c70a4231537554addd526625360aa7a70c01c0
hydro <ihydrodog@gmail.com>
2019-01-06 22:29:45 +0900

    save progress

--- Minimum Window Substring.py
+++ Minimum Window Substring.py
@@ -1,43 +1,91 @@
 from collections import Counter
-
+import math
 
 class Solution:
 
+    # def minWindow(self, s, t):
+    #     """
+    #     :type s: str
+    #     :type t: str
+    #     :rtype: str
+    #     """
+    #
+    #     posMap = {}
+    #     freq = Counter(t)
+    #
+    #     lenS = len(s)
+    #     lenT = len(t)
+    #
+    #     minWindowIndex, minWindowWidth = None, lenS + 1
+    #     indexQueue = []
+    #
+    #     for index in range(lenS):
+    #         c = s[index]
+    #         if c in t:
+    #             indexes = posMap.get(c, None)
+    #             if indexes != None and len(indexes) == freq[c]:
+    #                 indexQueue.remove(indexes.pop(0))
+    #
+    #             indexQueue.append(index)
+    #
+    #             posMap.setdefault( c, [] ).append( index )
+    #
+    #             minIndex = indexQueue[0]
+    #
+    #             if len(indexQueue) == lenT:
+    #                 if index - minIndex < minWindowWidth:
+    #                     minWindowIndex = minIndex
+    #                     minWindowWidth = index - minIndex
+    #
+    #     return s[minWindowIndex:minWindowIndex + minWindowWidth + 1] if minWindowIndex != None else ''
+
     def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+        filteredS = []
 
-        posMap = {}
-        freq = Counter(t)
+        lenS = len( s )
 
-        lenS = len(s)
-        lenT = len(t)
-
-        minWindowIndex, minWindowWidth = None, lenS + 1
-        indexQueue = []
-
-        for index in range(lenS):
+        for index in range( lenS ):
             c = s[index]
             if c in t:
-                indexes = posMap.get(c, None)
-                if indexes != None and len(indexes) == freq[c]:
-                    indexQueue.remove(indexes.pop(0))
+                filteredS.append( ( c, index) )
 
-                indexQueue.append(index)
+        countMap = Counter( t )
+        formed = 0
+        counter = {}
 
-                posMap.setdefault( c, [] ).append( index )
+        leftIndex, rightIndex = 0, 0
+        min = ( math.inf, 0, 0 )
+        while rightIndex < len( filteredS ):
+            c, r = filteredS[rightIndex]
+            rightIndex += 1
+            counter[c] = counter.get( c, 0 )+1
+            if counter[c] == countMap[c]:
+                formed += 1
 
-                minIndex = indexQueue[0]
+                if formed == len( countMap ):
 
-                if len(indexQueue) == lenT:
-                    if index - minIndex < minWindowWidth:
-                        minWindowIndex = minIndex
-                        minWindowWidth = index - minIndex
+                    curWidth = r - filteredS[leftIndex][1] + 1
 
-        return s[minWindowIndex:minWindowIndex + minWindowWidth + 1] if minWindowIndex != None else ''
+                    if curWidth < min[0]:
+                        min = (curWidth, filteredS[leftIndex][1], r)
+
+                    # advance leftIndex
+                    while leftIndex < rightIndex:
+                        c, left = filteredS[leftIndex]
+                        counter[c] -= 1
+                        leftIndex += 1
+                        if counter[c] < countMap[c]:
+                            formed -= 1
+                            break
+
+                        curWidth = r - filteredS[leftIndex][1] + 1
+                        if curWidth < min[0]:
+                            min = (curWidth, filteredS[leftIndex][1], r)
+
+
+        return s[min[1]:min[2]+1] if min[0] != math.inf else ''
+
+
 
 print( Solution().minWindow( "bbaa", "aba" ))
 
